1f740c2b3f93c57217082c0134bc2970d54147c6
Geekslife95 <geekslifesolutions@gmail.com>
2024-10-06 00:27:28 +0530

    ad

--- resources/views/user/court-booking/top-bar.blade.php
+++ resources/views/user/court-booking/top-bar.blade.php
@@ -9,7 +9,7 @@
         <a href="{{url('user/court-book-description')}}" class="btn btn-outline-primary @if(Request::url() === url('/').'/user/court-book-description') {{"active"}} @endif w-100  step-btn">3. Description</a>
     </div>
     <div class="col-lg-3 col-md-3 col-6">
-        <a href="{{url('user/court-booking')}}" class="btn btn-outline-primary @if(Request::url() === url('/').'/user/court-booking') {{"active"}} @endif w-100  step-btn">4. Images</a>
+        <a href="{{url('user/court-book-images')}}" class="btn btn-outline-primary @if(Request::url() === url('/').'/user/court-book-images') {{"active"}} @endif w-100  step-btn">4. Images</a>
     </div>
    
 </div>
