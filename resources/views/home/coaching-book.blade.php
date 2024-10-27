09c9ae09647298a1e4a96695e672ef6fe1070651
Geekslife95 <geekslifesolutions@gmail.com>
2024-10-27 22:52:21 +0530

    ad

--- resources/views/home/coaching-book.blade.php
+++ resources/views/home/coaching-book.blade.php
@@ -301,7 +301,7 @@
                     
                 @php
                     $sessionDurationData = json_decode($coachData->session_duration, true);
-                    dd($coachData->session_duration);
+                    dd($coachData);
                 @endphp
                 <!-- Intensity and Calories Section -->
                 <div class="mt-4 mb-3">
