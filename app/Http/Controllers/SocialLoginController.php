aee7a0542fbef3ca48dce9f20313936b5c63e999
Geekslife95 <geekslifesolutions@gmail.com>
2024-11-16 20:13:10 +0530

    s

--- app/Http/Controllers/SocialLoginController.php
+++ app/Http/Controllers/SocialLoginController.php
@@ -42,34 +42,39 @@ class SocialLoginController extends Controller
     
 
     public function googleAuthLogin(){
-        $user = Socialite::driver('google')->user();
-        if($user){
-            $checkUser = AppUser::select('id')->where(['social_id'=>$user->id,'login_type'=>'1'])->first();
-            if($checkUser){
-                $userId = $checkUser->id;
-            }else{
-                $data['name'] = $user->user['given_name'];
-                $data['last_name'] = $user->user['family_name'];
-                $data['email'] = $user->email;
-                $data['password'] = \Hash::make('GAUTH');
-                $data['image'] = "defaultuser.png";
-                $data['status'] = 1;
-                $data['provider'] = "LOCAL";
-                $data['language'] = 'English';
-                $data['phone'] = null;
-                $data['is_verify'] = 1;
-                $data['social_id'] = $user->id;
-                $data['login_type'] = 1;
-                $data['created_at'] = date("Y-m-d H:i:s");
-                $data['created_at'] = date("Y-m-d H:i:s");
-                $userId = AppUser::insertGetId($data);
+        try{
+            $user = Socialite::driver('google')->user();
+            if($user){
+                $checkUser = AppUser::select('id')->where(['social_id'=>$user->id,'login_type'=>'1'])->first();
+                if($checkUser){
+                    $userId = $checkUser->id;
+                }else{
+                    $data['name'] = $user->user['given_name'];
+                    $data['last_name'] = $user->user['family_name'];
+                    $data['email'] = $user->email;
+                    $data['password'] = \Hash::make('GAUTH');
+                    $data['image'] = "defaultuser.png";
+                    $data['status'] = 1;
+                    $data['provider'] = "LOCAL";
+                    $data['language'] = 'English';
+                    $data['phone'] = null;
+                    $data['is_verify'] = 1;
+                    $data['social_id'] = $user->id;
+                    $data['login_type'] = 1;
+                    $data['created_at'] = date("Y-m-d H:i:s");
+                    $data['created_at'] = date("Y-m-d H:i:s");
+                    $userId = AppUser::insertGetId($data);
+                }
+                Auth::guard('appuser')->loginUsingId($userId);
+                if(Session::has('LAST_URL_VISITED')){
+                    $redirectLink = Session::get('LAST_URL_VISITED');
+                    return redirect($redirectLink);
+                }
             }
-            Auth::guard('appuser')->loginUsingId($userId);
-            if(Session::has('LAST_URL_VISITED')){
-                $redirectLink = Session::get('LAST_URL_VISITED');
-                return redirect($redirectLink);
-            }
+            return redirect('/');
+        }catch(\Exception $e){
+            return "LOGIN ERROR - ".$e->getMessage();
         }
-        return redirect('/');
+       
     }
 }
